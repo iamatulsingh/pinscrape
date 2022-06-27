5449d342cba4f635752c06e07eff46f25ae41167
Atul Singh <atulsingh0401@gmail.com>
2022-06-27 17:51:12 +0200

    fix: let code throw error for user understanding

--- pinscrape/__init__.py
+++ pinscrape/__init__.py
@@ -3,7 +3,7 @@ from ._version import __version__ as _version
 
 __version__ = _version
 __author__ = "Atul Singh"
-__copyright__ = "Copyright 2021"
+__copyright__ = "Copyright © 2021-2022"
 __license__ = "MIT"
 __maintainer__ = "Atul Singh"
 __email__ = "atulsingh0401@gmail.com"

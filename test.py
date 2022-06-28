a301fa1f1d04c843d21b151fc79ec60373828755
Atul Singh <atulsingh0401@gmail.com>
2022-06-28 10:53:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ details = pinscrape.scraper.scrape("messi", "output", {}, 10)
 
 if details["isDownloaded"]:
     print("\nDownloading completed !!")
+    print(f"\nTotal urls found: {len(details['extracted_urls'])}")
+    print(f"\nTotal images downloaded (including duplicate images): {len(details['url_list'])}")
     print(details)
 else:
     print("\nNothing to download !!")

40c280ccb3a03f0f2c382f6640bc4a829da2c6cd
Jake <112051772+Jakee8718@users.noreply.github.com>
2023-06-04 18:52:43 -0400

    Delete __init__.py

--- pinscrape/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._version import __version__ as _version
-
-
-__version__ = _version
-__author__ = "Atul Singh"
-__copyright__ = "Copyright © 2021-2022"
-__license__ = "MIT"
-__maintainer__ = "Atul Singh"
-__email__ = "atulsingh0401@gmail.com"
